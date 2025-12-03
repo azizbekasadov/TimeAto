06f4359188a9a8ef9de6c311d1430d8d2ab3ca3e
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-12-03 15:01:23 +0100

    Initial Commit

---
+++ TimeAto/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TimeAto
+//
+//  Created by Azizbek Asadov on 03.12.2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
